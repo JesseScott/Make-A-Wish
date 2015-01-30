9d0566e6c2d9b6fb963ea609d371cf0bdb424a48
JesseScott <jessecolinscott@gmail.com>
2015-01-30 00:04:56 -0800

    cordova plugin add org.apache.cordova.device

--- makeawish/platforms/ios/www/cordova_plugins.js
+++ makeawish/platforms/ios/www/cordova_plugins.js
@@ -20,13 +20,21 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.splashscreen": "0.3.5",
-    "org.apache.cordova.console": "0.2.12"
+    "org.apache.cordova.console": "0.2.12",
+    "org.apache.cordova.device": "0.2.13"
 }
 // BOTTOM OF METADATA
 });

938109d838764a6bdec0eb2e9804103f2d3f34a7
Jesse Scott <jesse.scott@eventbase.com>
2017-02-24 00:01:19 -0800

    Fixing notifications

---
+++ MakeAWish/app/src/main/java/tt/co/jesses/makeawish/NotificationActivity.java
@@ -0,0 +1,18 @@
+package tt.co.jesses.makeawish;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jessescott on 2017-02-22.
+ */
+
+public class NotificationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+
+}

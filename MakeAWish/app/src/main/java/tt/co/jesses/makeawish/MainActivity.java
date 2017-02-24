55ac319ac2941558097d57795b5d478d030547cf
Jesse Scott <jesse.scott@eventbase.com>
2017-02-24 00:01:54 -0800

    Refactoring / Modularizing MainActivity

--- MakeAWish/app/src/main/java/tt/co/jesses/makeawish/MainActivity.java
+++ MakeAWish/app/src/main/java/tt/co/jesses/makeawish/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -16,34 +18,22 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private Calendar[] calendarsAM = new Calendar[6];
-    // AM - 12:12, 1:11, 2:22, 3:33, 4:44, 5:55
-    private Calendar[] calendarsPM = new Calendar[6];
-    // PM - 12:12, 1:11, 2:22, 3:33, 4:44, 5:55
+    public static final String PREFS_NAME = "MAW_PREFS";
+    private SharedPreferences mPreferences;
+
+    private Calendar[] calendarsNighttime = new Calendar[8];
+    // AM - 10:10, 11:11, 12:12, 1:11, 2:22, 3:33, 4:44, 5:55
+    private Calendar[] calendarsDaytime = new Calendar[8];
+    // PM - 10:10, 11:11, 12:12, 1:11, 2:22, 3:33, 4:44, 5:55
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // https://developer.android.com/guide/topics/ui/notifiers/notifications.html
-        // https://developer.android.com/training/scheduling/alarms.html
+        mPreferences = getPreferences(MODE_PRIVATE);
 
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-
-        Intent notificationIntent = new Intent("android.media.action.DISPLAY_NOTIFICATION");
-        notificationIntent.addCategory("android.intent.category.DEFAULT");
-
-        PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.SECOND, 15);
-        //alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), broadcast);
-
-        setCalendarsPM();
-        for (Calendar calendar : calendarsPM) {
-            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, broadcast);
-        }
+        setAlarms();
 
     }
 
@@ -67,88 +57,150 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void setCalendarsAM() {
+    private void setAlarms() {
+
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean daytime = sharedPref.getBoolean(getString(R.string.settings_enable_daytime_alarms), false);
+
+
+        // Test to see if we've already set recurring daytime alarms AND/OR if we have enabled them
+        boolean daytimeEnabled = mPreferences.getBoolean(getString(R.string.settings_enable_daytime_alarms), false);
+        boolean daytimeSet = mPreferences.getBoolean(getString(R.string.prefs_daytime_set), false);
+//        if (!daytimeSet && daytimeEnabled) {
+//            setCalendarsDaytime();
+//            for (Calendar calendar : calendarsDaytime) {
+//                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmIntent);
+//            }
+//            // Set the Preferences
+//            mPreferences.edit().putBoolean(getString(R.string.prefs_daytime_set), true).apply();
+//        }
+
+        // Test to see if we've already set recurring nighttime alarms AND/OR if we have enabled them
+        boolean nighttimeEnabled = mPreferences.getBoolean(getString(R.string.settings_enable_nighttime_alarms), false);
+        boolean nighttimeSet = mPreferences.getBoolean(getString(R.string.prefs_nighttime_set), false);
+//        if (!nighttimeSet && nighttimeEnabled) {
+//            setCalendarsNighttime();
+//            for (Calendar calendar : calendarsNighttime) {
+//                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmIntent);
+//            }
+//            // Set the Preferences
+//            mPreferences.edit().putBoolean(getString(R.string.prefs_nighttime_set), true).apply();
+//        }
+
+
+    }
+
+    private void setCalendarsNighttime() {
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
 
+        // 10:10 PM
+        calendar.set(Calendar.HOUR_OF_DAY, 22);
+        calendar.set(Calendar.MINUTE, 10);
+        calendarsDaytime[0] = calendar;
+        calendar.clear();
+
+        // 11:11 PM
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 11);
+        calendarsDaytime[1] = calendar;
+        calendar.clear();
+
         // 12:12 AM
         calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 12);
-        calendarsAM[0] = calendar;
+        calendarsNighttime[2] = calendar;
         calendar.clear();
 
         // 1:11 AM
         calendar.set(Calendar.HOUR_OF_DAY, 1);
         calendar.set(Calendar.MINUTE, 11);
-        calendarsAM[1] = calendar;
+        calendarsNighttime[3] = calendar;
         calendar.clear();
 
         // 2:22 AM
         calendar.set(Calendar.HOUR_OF_DAY, 2);
         calendar.set(Calendar.MINUTE, 22);
-        calendarsAM[3] = calendar;
+        calendarsNighttime[4] = calendar;
         calendar.clear();
 
         // 3:33 AM
         calendar.set(Calendar.HOUR_OF_DAY, 3);
         calendar.set(Calendar.MINUTE, 33);
-        calendarsAM[4] = calendar;
+        calendarsNighttime[5] = calendar;
         calendar.clear();
 
         // 4:44 AM
         calendar.set(Calendar.HOUR_OF_DAY, 4);
         calendar.set(Calendar.MINUTE, 44);
-        calendarsAM[5] = calendar;
+        calendarsNighttime[6] = calendar;
         calendar.clear();
 
-        // 5:55
+        // 5:55 AM
         calendar.set(Calendar.HOUR_OF_DAY, 5);
         calendar.set(Calendar.MINUTE, 55);
-        calendarsAM[6] = calendar;
+        calendarsNighttime[7] = calendar;
         calendar.clear();
 
     }
 
-    private void setCalendarsPM() {
+    private void setCalendarsDaytime() {
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
 
+        // 10:10 AM
+        calendar.set(Calendar.HOUR_OF_DAY, 10);
+        calendar.set(Calendar.MINUTE, 10);
+        calendarsDaytime[0] = calendar;
+        calendar.clear();
+
+        // 11:11 AM
+        calendar.set(Calendar.HOUR_OF_DAY, 11);
+        calendar.set(Calendar.MINUTE, 11);
+        calendarsDaytime[1] = calendar;
+        calendar.clear();
+
         // 12:12 AM
         calendar.set(Calendar.HOUR_OF_DAY, 12);
         calendar.set(Calendar.MINUTE, 12);
-        calendarsPM[0] = calendar;
+        calendarsDaytime[2] = calendar;
         calendar.clear();
 
         // 1:11 AM
         calendar.set(Calendar.HOUR_OF_DAY, 13);
         calendar.set(Calendar.MINUTE, 11);
-        calendarsPM[1] = calendar;
+        calendarsDaytime[3] = calendar;
         calendar.clear();
 
         // 2:22 AM
         calendar.set(Calendar.HOUR_OF_DAY, 14);
         calendar.set(Calendar.MINUTE, 22);
-        calendarsPM[2] = calendar;
+        calendarsDaytime[4] = calendar;
         calendar.clear();
 
         // 3:33 AM
         calendar.set(Calendar.HOUR_OF_DAY, 15);
         calendar.set(Calendar.MINUTE, 33);
-        calendarsPM[3] = calendar;
+        calendarsDaytime[5] = calendar;
         calendar.clear();
 
         // 4:44 AM
         calendar.set(Calendar.HOUR_OF_DAY, 16);
         calendar.set(Calendar.MINUTE, 44);
-        calendarsPM[4] = calendar;
+        calendarsDaytime[6] = calendar;
         calendar.clear();
 
         // 5:55
         calendar.set(Calendar.HOUR_OF_DAY, 17);
         calendar.set(Calendar.MINUTE, 55);
-        calendarsPM[5] = calendar;
+        calendarsDaytime[7] = calendar;
         calendar.clear();
 
     }

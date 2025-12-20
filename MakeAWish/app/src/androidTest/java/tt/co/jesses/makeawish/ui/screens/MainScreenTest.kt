dbb9b881bdbd3a888563c71f55838defbf64195e
Jesse Scott <jessecolinscott@gmail.com>
2025-12-20 00:05:01 -0800

    Fix UI Test

--- MakeAWish/app/src/androidTest/java/tt/co/jesses/makeawish/ui/screens/MainScreenTest.kt
+++ MakeAWish/app/src/androidTest/java/tt/co/jesses/makeawish/ui/screens/MainScreenTest.kt
@@ -1,54 +1,46 @@
 package tt.co.jesses.makeawish.ui.screens
 
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
-import androidx.compose.ui.test.performImeAction
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import tt.co.jesses.makeawish.MainActivity
+import tt.co.jesses.makeawish.R
 
 @RunWith(AndroidJUnit4::class)
 class MainScreenTest {
 
     @Rule
     @JvmField
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
     fun testAddWishDialogOpensAndContentDescriptionIsCorrect() {
-        composeTestRule.setContent {
-            MainScreen(onSettingsClick = {})
-        }
-
         // Click the FAB using the new localized content description
-        // Note: In a real test we should use the resource ID, but for simplicity here we use the string value
-        // that we know we added to strings.xml: "Add a new wish"
-        composeTestRule.onNodeWithContentDescription("Add a new wish").performClick()
+        composeTestRule.onNodeWithContentDescription(composeTestRule.activity.getString(R.string.cd_add_wish)).performClick()
 
         // Verify the dialog title exists
-        composeTestRule.onNodeWithText("Make a New Wish").assertExists()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.dialog_title_make_wish)).assertExists()
     }
 
     @Test
     fun testEnterWishAndSave() {
-        composeTestRule.setContent {
-            MainScreen(onSettingsClick = {})
-        }
-
+        val input = "My wish"
         // Open Dialog
-        composeTestRule.onNodeWithContentDescription("Add a new wish").performClick()
+        composeTestRule.onNodeWithContentDescription(composeTestRule.activity.getString(R.string.cd_add_wish)).performClick()
 
         // Enter text
-        composeTestRule.onNodeWithText("Enter your wish").performTextInput("My Wish")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.dialog_label_enter_wish)).performTextInput(input)
 
-        // Perform IME action (Done) which should trigger save and close dialog
-        composeTestRule.onNodeWithText("My Wish").performImeAction()
+        // Perform save and close dialog
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.dialog_btn_save)).performClick()
 
         // Verify dialog is closed (Title no longer exists)
-        composeTestRule.onNodeWithText("Make a New Wish").assertDoesNotExist()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.dialog_title_make_wish)).assertDoesNotExist()
     }
 }
